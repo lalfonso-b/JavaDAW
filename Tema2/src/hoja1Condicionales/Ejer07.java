342d42521204f8f6fe47f7644078e79667d3f37e
profesorderedes <profesorderedes@gmail.com>
2023-10-26 16:53:17 +0200

    jue 26 oct 2023 16:53:17 CEST

---
+++ Tema2/src/hoja1Condicionales/Ejer07.java
@@ -0,0 +1,41 @@
+package hoja1Condicionales;
+
+import java.util.Scanner;
+
+public class Ejer07 {
+
+	public static void main(String[] args) {
+
+		/*
+		 * 
+		 * Juego de adivinar el número secreto. El ordenador nos preguntará un número
+		 * 
+		 * del 1 al 10. Si coincide con el número secreto (que habrás codificado como
+		 * 
+		 * una constante entera) nos felicitará. Si no, nos dirá que hemos fallado.
+		 * 
+		 */
+
+		Scanner escaner = new Scanner(System.in);
+
+		System.out.println("Escribe un número del 1 al 10");
+
+		int num = escaner.nextInt();
+
+		final int NUM_SECRETO = 3;
+
+		if (num == NUM_SECRETO) {
+
+			System.out.println("De locos, acertaste!");
+
+		} else {
+
+			System.out.println("F  :(");
+
+		}
+
+		escaner.close();
+
+	}
+
+}
