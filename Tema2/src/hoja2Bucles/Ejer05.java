e48ab94c2c5e02e620b1197e4188870cc34cb7df
profesorderedes <profesorderedes@gmail.com>
2023-10-27 16:55:21 +0200

    vie 27 oct 2023 16:55:21 CEST

---
+++ Tema2/src/hoja2Bucles/Ejer05.java
@@ -0,0 +1,16 @@
+package hoja2Bucles;
+
+/*
+Escribe un programa que solicite un número al usuario y, a continuación, muestre la tabla de
+multiplicar de ese número.
+*/
+
+public class Ejer05 {
+	
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
