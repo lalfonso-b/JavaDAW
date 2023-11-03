624debe12faea8f07bcf2abef137ecbbec223e11
profesorderedes <profesorderedes@gmail.com>
2023-11-03 16:33:01 +0100

    vie 03 nov 2023 16:33:00 CET

---
+++ Tema2/src/ejemplosBucles/Ejemplo09.java
@@ -0,0 +1,31 @@
+package ejemplosBucles;
+
+public class Ejemplo09 {
+	
+	public static void main(String[] args) {
+		
+		// El método Math.random() devuelve un número mayor o
+		// igual a 0, y menor que 1.
+		
+		int contador = 1;
+		while(contador <= 10) {
+			System.out.println(Math.random());
+			contador++;
+		}
+		
+		// Si mutiplicamos Math.random() por 2 y le sumamos 1, obtendremos
+		// números decimales mayores o iguales a 1 y menores que 3.
+		
+		System.out.println("\n");
+		
+		contador = 1;
+		while(contador <= 10) {
+			int numAleatorio = (int)(10 * Math.random() + 1);
+			System.out.println(numAleatorio);
+			contador++;
+		}
+		
+		
+	}
+
+}
