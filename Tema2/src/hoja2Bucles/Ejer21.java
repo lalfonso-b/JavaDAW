65cbe0d545ce97a154834c4f43d83510850f529e
profesorderedes <profesorderedes@gmail.com>
2023-11-08 19:20:26 +0100

    mié 08 nov 2023 19:20:26 CET

---
+++ Tema2/src/hoja2Bucles/Ejer21.java
@@ -0,0 +1,26 @@
+package hoja2Bucles;
+
+import java.util.Scanner;
+
+public class Ejer21 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		
+		int entero = 0;
+		for (int i = 1; i <= 5; i++) {
+
+			System.out.println("INTRODUCE EL NÚMERO ENTERO:");
+			entero = sc.nextInt();
+
+		}
+
+		System.out.println("Has fallado,el número era " + entero);
+
+		sc.close();
+
+	}
+
+}
