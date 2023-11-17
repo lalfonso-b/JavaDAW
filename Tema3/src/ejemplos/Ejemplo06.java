7321eba24ae6560f3b7d834f81d22cffc83cb91c
profesorderedes <profesorderedes@gmail.com>
2023-11-17 17:00:02 +0100

    vie 17 nov 2023 17:00:02 CET

---
+++ Tema3/src/ejemplos/Ejemplo06.java
@@ -0,0 +1,40 @@
+package ejemplos;
+
+import java.util.Scanner;
+
+public class Ejemplo06 {
+
+	public static void main(String[] args) {
+
+		Scanner entrada = new Scanner(System.in);
+
+		double[] tallas = { 37, 40, 40.5, 39, 43, 45, 47, 41, 40 };
+
+		// Leemos una talla desde la terminal.
+		System.out.println("Indique una talla y le diré si está en el " + "array:");
+		double talla = entrada.nextDouble();
+
+		boolean encontrado = false;
+
+		// Buscamos si la talla se encuentra. Si está, diremos en qué
+		// posición y si no, lo indicaremos.
+		int i;
+		for (i = 0; i < tallas.length; i++) {
+
+			if (tallas[i] == talla) {
+				encontrado = true;
+				break;
+			}
+
+		}
+
+		if (encontrado) {
+			System.out.println("Tu talla se encuentra en la posición " 
+					+ i + ".");
+		} else {
+			System.out.println("Tu talla no se ha encontrado.");
+		}
+
+	}
+
+}
