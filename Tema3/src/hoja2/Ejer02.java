93ba8523b6aa3797e89410838b6f8223a3fde97a
profesorderedes <profesorderedes@gmail.com>
2023-11-21 20:04:35 +0100

    mar 21 nov 2023 20:04:35 CET

---
+++ Tema3/src/hoja2/Ejer02.java
@@ -0,0 +1,38 @@
+package hoja2;
+
+import java.util.Scanner;
+
+public class Ejer02 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		double[] estaturas = new double[10];
+
+		System.out.println("Introduce las estaturas: ");
+
+		for (int i = 0; i < estaturas.length; i++) {
+
+			System.out.println("Estatura " + (i + 1));
+			estaturas[i] = sc.nextDouble();
+
+		}
+
+		for (int i = 0; i < estaturas.length; i++) {
+
+			if (estaturas[i] > 1.90) {
+
+				System.out.println("\nSe ha encontrado una estatura de "
+						+ "más de 1'90");
+				break;
+
+			}
+
+		}
+
+		sc.close();
+
+	}
+
+}
