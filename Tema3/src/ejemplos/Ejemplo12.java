d78a22c288c472f06dcee3701254c00983772bf1
profesorderedes <profesorderedes@gmail.com>
2023-11-29 19:27:17 +0100

    mié 29 nov 2023 19:27:16 CET

--- Tema3/src/ejemplos/Ejemplo12.java
+++ Tema3/src/ejemplos/Ejemplo12.java
@@ -1,16 +1,36 @@
 package ejemplos;
 
-// Actividad: Escribe una tabla con edades y alturas de 
-// un grupo de personas
-// 		Edad	Altura
-//		12		1,50
-// La tabla se llamará datos (al menos 4 filas).
-// Múestrala en forma de tabla.
-
 public class Ejemplo12 {
-	
+
 	public static void main(String[] args) {
-		
+
+		// Actividad: Escribe una tabla con edades y altura de
+		// un grupo de personas
+		// Edad Altura
+		// 12 1,50
+		// La tabla se llamará datos ( al menos 4 filas).
+		// Múestrala en forma de tabla.
+
+		double[][] datos = {
+				{ 12, 1.50 },
+				{ 15, 1.75 },
+				{ 21, 1.69 },
+				{ 32, 1.90 } 
+			};
+
+		System.out.println("Edad\tAltura\n");
+
+		for (int fila = 0; fila < datos.length; fila++) {
+
+			// Para cada fila mostramos sus columnas.
+			for (int col = 0; col < datos[0].length; col++) {
+				System.out.print(datos[fila][col] + "\t");
+			}
+
+			System.out.println();
+
+		}
+
 	}
 
 }
