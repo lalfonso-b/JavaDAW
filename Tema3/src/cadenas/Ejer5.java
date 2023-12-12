062b7942cc6c35c7214068993dbb8b33bb500a08
profesorderedes <profesorderedes@gmail.com>
2023-12-12 20:47:55 +0100

    mar 12 dic 2023 20:47:54 CET

---
+++ Tema3/src/cadenas/Ejer5.java
@@ -0,0 +1,31 @@
+package cadenas;
+
+import java.util.Scanner;
+
+public class Ejer5 {
+	public static void main(String[] args) {
+
+		/*
+		 * Programa que lee una frase y nos dice por cuántas palabras está formada.
+		 */
+		Scanner escaner = new Scanner(System.in);
+
+		String frase;
+		int contador = 1;
+
+		System.out.println("Introduce una frase: ");
+		frase = escaner.nextLine();
+
+		frase = frase.trim();
+		
+		for (int i = 0; i < frase.length(); i++) {
+			if (frase.charAt(i) == ' ') {
+				contador++;
+			}
+		}
+
+		System.out.println("La frase consta de: " + contador + " palabras.");
+		escaner.close();
+		
+	}
+}
---
+++ Tema3/src/cadenasEjemplos/Ejer07.java
@@ -0,0 +1,15 @@
+package cadenasEjemplos;
+
+public class Ejer07 {
+	
+	public static void main(String[] args) {
+		
+		String frase = "Volvía a casa de noche y fui asaltado por un "
+				+ "sinvergüenza.";
+		String[] palabrotas = {"sinvergüenza", "maleducado", "analfabeto", 
+				"botarate", "bribón", "mendrugo"};
+		
+		
+	}
+
+}
