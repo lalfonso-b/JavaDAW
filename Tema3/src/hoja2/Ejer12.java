3da0d8819672aabfca9d2fc2b1b2b96477108e9d
profesorderedes <profesorderedes@gmail.com>
2023-12-12 19:10:29 +0100

    mar 12 dic 2023 19:10:29 CET

---
+++ Tema3/src/hoja2/Ejer12.java
@@ -0,0 +1,42 @@
+package hoja2;
+
+public class Ejer12 {
+
+	public static void main(String[] args) {
+
+		int numAtom[] = { 8, 2, 10, 20, 12, 71, 15, 23 };
+		int min = 0;
+		int aux = 0;
+
+		System.out.println("Números atómicos ordenados de menor a mayor:");
+
+		for (int i = 0; i < numAtom.length - 1; i++) {
+
+			min = i;
+
+			for (int j = i + 1; j < numAtom.length; j++) {
+
+				if (numAtom[j] < numAtom[min]) {
+					min = j;
+				}
+
+			}
+
+			aux = numAtom[i];
+			numAtom[i] = numAtom[min];
+			numAtom[min] = aux;
+
+			System.out.println(numAtom[i]);
+
+		}
+		
+		// Mostramos la última posición.
+		System.out.println(numAtom[numAtom.length-1]);
+
+		System.out.println("\nNúmero atómico mayor: " 
+				+ numAtom[numAtom.length - 1]);
+		System.out.println("Número atómico menor: " + numAtom[0]);
+
+	}
+
+}
