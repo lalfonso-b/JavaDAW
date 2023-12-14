4647ef985163ceb9f08d2ddd5236b392c1a5c83b
profesorderedes <profesorderedes@gmail.com>
2023-12-14 16:54:29 +0100

    jue 14 dic 2023 16:54:29 CET

--- Tema3/src/cadenas/Ejer16.java
+++ Tema3/src/cadenas/Ejer16.java
@@ -11,7 +11,21 @@ public class Ejer16 {
 		System.out.println("Escribe una frase y te genero su acrónimo:");
 		String frase = entrada.nextLine();
 		
+		// Separamos la frase en palabras sueltas guardadas en un array.
+		
+		
+		// Formamos una cadena con la primera letra de cada palabra.
+		
+		
+		// Pasamos esa cadena a mayúsculas.
+		
 		
 	}
 
 }
+
+
+
+
+
+
