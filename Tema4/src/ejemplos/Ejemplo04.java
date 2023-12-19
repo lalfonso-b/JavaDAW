ac80f7ffc526417564543364e1d3da39c0322d4e
profesorderedes <profesorderedes@gmail.com>
2023-12-19 19:46:24 +0100

    mar 19 dic 2023 19:46:24 CET

---
+++ Tema4/src/ejemplos/Ejemplo04.java
@@ -0,0 +1,20 @@
+package ejemplos;
+
+public class Ejemplo04 {
+
+	public static void main(String[] args) {
+
+		System.out.println("El doble de 15 es " + doblar(15));
+
+		int doble = doblar(20);
+		System.out.println("El doble de 20 es " + doble);
+
+	}
+
+	// El método doblar recibe un argumento, num, de tipo entero.
+	// Devuelve el doble de num.
+	static int doblar(int num) {
+		return 2 * num;
+	}
+
+}
---
+++ Tema4/src/ejemplos/Ejemplo06.java
@@ -0,0 +1,13 @@
+package ejemplos;
+
+public class Ejemplo06 {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	// Muestra la tabla de multiplicar del número que le pases.
+	static void tablaMultiplicar(int num) {
+		
+	}
+}
