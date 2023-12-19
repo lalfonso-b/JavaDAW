9084a846f0bc4b579fd787e9ef998c9a9308f630
profesorderedes <profesorderedes@gmail.com>
2023-12-19 18:52:42 +0100

    mar 19 dic 2023 18:52:42 CET

--- Tema3/src/ejemplos/Ejemplo16.java
+++ Tema3/src/ejemplos/Ejemplo16.java
@@ -4,7 +4,30 @@ public class Ejemplo16 {
 	
 	public static void main(String[] args) {
 		
-		String[][] alturas = { { "Manuel", "1.75" }, { "Rosa", "1.70" }, { "María", "1.65" }, { "Jorge", "1.72" } };
+		String[][] alturas = { 
+				{ "Manuel Díaz", "1.75" }, 
+				{ "Rosa Sánchez", "1.70" }, 
+				{ "María de la Rosa", "1.65" }, 
+				{ "Jorge Luis Martínez", "1.72" } 
+			};
+		
+		// Ejemplo de uso de printf.
+		float temp = 10;
+		float humedad = 60;
+		System.out.printf("Hoy hace %f grados y hay una humedad de %f %% \n"
+				, temp, humedad);
+		
+		System.out.printf("\n%-20s %4s\n", "Nombre", "Estatura");
+		System.out.printf("%-20s %4s\n", "====================", "========");
+		
+		for (int fila = 0; fila < alturas.length; fila++) {
+			
+			System.out.printf("%-20s %4s\n", alturas[fila][0], 
+					alturas[fila][1]);
+			
+		}
+		
+		
 
 		
 	}
