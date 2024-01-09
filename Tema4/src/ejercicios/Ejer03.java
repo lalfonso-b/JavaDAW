c692881fff0b731715dc6b979ca33914561f34f5
profesorderedes <profesorderedes@gmail.com>
2024-01-09 19:51:37 +0100

    mar 09 ene 2024 19:51:37 CET

--- Tema4/src/ejercicios/Ejer03.java
+++ Tema4/src/ejercicios/Ejer03.java
@@ -6,7 +6,7 @@ public class Ejer03 {
 
 	public static void main(String[] args) {
 
-		System.out.println("Te llamas " + preguntarNombre());
+		System.out.println("Te llamas " + preguntarNombre() + ".");
 
 	}
 
