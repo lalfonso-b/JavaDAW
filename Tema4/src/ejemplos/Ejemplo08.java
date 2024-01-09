3286916444bfcd86bc62800a69beb51fa0e9ef43
profesorderedes <profesorderedes@gmail.com>
2024-01-09 19:18:26 +0100

    mar 09 ene 2024 19:18:26 CET

--- Tema4/src/ejemplos/Ejemplo08.java
+++ Tema4/src/ejemplos/Ejemplo08.java
@@ -1,11 +1,18 @@
 package ejemplos;
 
 public class Ejemplo08 {
-	
+
 	public static void main(String[] args) {
-		
+
+		System.out.println("El área de un triángulo de base 20 y altura 5 es " + areaTriangulo(20, 5));
+
 	}
 
-	// Devuelve el área de un triángulo con los argumentos de base y altura recibidos.
-	
+	// Devuelve el área de un triángulo con los argumentos de base y altura
+	// recibidos.
+	static double areaTriangulo(double base, double altura) {
+
+		return base * altura / 2;
+
+	}
 }
---
+++ Tema4/src/ejercicios/Ejer01.java
@@ -0,0 +1,13 @@
+package ejercicios;
+
+public class Ejer01 {
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	static void saludar(String nombre, int edad) {
+		
+	}
+
+}
