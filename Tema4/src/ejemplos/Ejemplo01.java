65e40be0d98be9934dbeaf73d08965c3d6a8e271
profesorderedes <profesorderedes@gmail.com>
2024-01-09 18:53:21 +0100

    mar 09 ene 2024 18:53:21 CET

--- Tema4/src/ejemplos/Ejemplo01.java
+++ Tema4/src/ejemplos/Ejemplo01.java
@@ -1,22 +1,22 @@
 package ejemplos;
 
 public class Ejemplo01 {
-	
+
 	public static void main(String[] args) {
-		
+
 		System.out.println("Llamando al mayordomo.");
-		
+
 		// Llamamos al método mayordomo().
 		mayordomo();
 		System.out.println("Tráigame unos turrones.");
-		
+
 		// Volvemos a llamar al método mayordomo().
 		mayordomo();
 		mayordomo();
-		
+
 	}
-	
-	// Definimos el método mayordomo(). 
+
+	// Definimos el método mayordomo().
 	// Este método no devuelve nada y no toma ningún argumento.
 	static void mayordomo() {
 		System.out.println("¿Qué desea, señor?");
---
+++ Tema4/src/ejemplos/Ejemplo08.java
@@ -0,0 +1,11 @@
+package ejemplos;
+
+public class Ejemplo08 {
+	
+	public static void main(String[] args) {
+		
+	}
+
+	// Devuelve el área de un triángulo con los argumentos de base y altura recibidos.
+	
+}
