1ae0684ee8f740bbcbc9025395febcca9f3a2636
profesorderedes <profesorderedes@gmail.com>
2024-01-16 18:48:23 +0100

    mar 16 ene 2024 18:48:23 CET

--- Tema4/src/ejercicios/Ejer10.java
+++ Tema4/src/ejercicios/Ejer10.java
@@ -3,19 +3,35 @@ package ejercicios;
 import java.util.Scanner;
 
 public class Ejer10 {
-
+	// Escribe un método que escriba en pantalla el nombre de un mes a partir
+	// de su número. En caso de que el número proporcionado no esté entre
+	// 1 y 12, deberá terminar inmediatamente.
 	public static void main(String[] args) {
 
+		int mesNumero;
+
 		Scanner entrada = new Scanner(System.in);
+		System.out.print("Introduce el número del mes:");
+		mesNumero = entrada.nextInt();
 
-		System.out.println("Dime un número de mes y te diré su nombre:");
-		int mes = entrada.nextInt();
+//		if(mesNumero > 12) {
+//			break;
+//		}
 
-		nombreMes(mes);
+		encontrarMes(mesNumero);
+	}
+
+	static void encontrarMes(int mesNumero) {
+
+		String[] mesNombres = { "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre",
+				"Octubre", "Noviembre", "Diciembre" };
+
+		if (mesNumero < 0 || mesNumero > 12) {
+			return;
+		}
+
+		System.out.println(mesNombres[mesNumero - 1]);
 
 	}
 
-	static void nombreMes(int numMes) {
-
-	}
 }
--- Tema4/src/ejercicios/Ejer11.java
+++ Tema4/src/ejercicios/Ejer11.java
@@ -6,19 +6,26 @@ public class Ejer11 {
 
 	public static void main(String[] args) {
 
-		Scanner entrada = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 
 		System.out.println("Dime un año y te diré si es bisiesto:");
-		int anyo = entrada.nextInt();
+		int anyo = sc.nextInt();
 
 		if (esBisiesto(anyo)) {
 			System.out.println("Es bisiesto.");
 		} else {
 			System.out.println("No es bisiesto.");
 		}
+		sc.close();
 	}
 
 	static boolean esBisiesto(int anyo) {
-		return false;
+
+		if ((anyo % 4 == 0 && anyo % 100 != 0) || (anyo % 400 == 0)) {
+			return true;
+		} else {
+			return false;
+		}
+
 	}
 }
