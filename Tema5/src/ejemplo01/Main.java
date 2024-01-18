39a61b2d693334a3a4992c07e2f05b438c8bc360
profesorderedes <profesorderedes@gmail.com>
2024-01-18 16:02:40 +0100

    jue 18 ene 2024 16:02:40 CET

--- Tema5/src/ejemplo01/Main.java
+++ Tema5/src/ejemplo01/Main.java
@@ -17,31 +17,42 @@ public class Main {
 		// Queremos mostrar algunos datos de persona1.
 		System.out.println("Nuestra persona se llama " + persona1.nombre + " " + persona1.apellidos + ".");
 		System.out.println("Nació el " + persona1.fechaNacimiento + ".");
-		
+
 		// Podemos cambiar cualquier propiedad.
 		persona1.apellidos = "Sánchez Pérez";
 		System.out.println("Nuestra persona se llama " + persona1.nombre + " " + persona1.apellidos + ".");
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
+		// Podemos crear tantos objetos Persona como necesitemos.
+		Persona persona2; // Declaramos la variable de tipo Persona
+		persona2 = new Persona(); // Le asignamos un nuevo objeto de tipo Persona
+
+		persona2.apellidos = "Martínez López";
+		persona2.nombre = "María";
+		System.out.println("Nuestra persona 2 se llama " + persona2.nombre + " " + persona2.apellidos + ".");
+
+		// Cuando no damos un valor a una propiedad ésta recibe un valor por defecto. Si
+		// es un objeto, valdrá null, si es un número, valdrá 0 y si es un boolean,
+		// valdrá false.
+		System.out.println("María nació el " + persona2.fechaNacimiento + ".");
+
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Tema5/src/ejemplo01/Persona.java
+++ Tema5/src/ejemplo01/Persona.java
@@ -1,11 +1,18 @@
 package ejemplo01;
 
 public class Persona {
-	
-	// Propiedades de la clase.
+
+	// Propiedades o atributos de la clase.
 	String apellidos;
 	String nombre;
 	String dni;
 	String fechaNacimiento;
+
+	// Métodos.
+	void mostrar() {
+		System.out.println("Nombre completo: " + apellidos + " " + nombre + ".");
+		System.out.println("DNI: " + dni + ".");
+		System.out.println("Fecha de nacimiento: " + fechaNacimiento + ".");
+	}
 
 }
