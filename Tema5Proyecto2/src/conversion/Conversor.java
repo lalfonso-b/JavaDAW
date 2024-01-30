5f73242e7269c8e6aeb5bba3d5af0fa9b0925947
profesorderedes <profesorderedes@gmail.com>
2024-01-30 18:36:02 +0100

    mar 30 ene 2024 18:36:02 CET

--- Tema5Proyecto2/src/conversion/Conversor.java
+++ Tema5Proyecto2/src/conversion/Conversor.java
@@ -2,12 +2,40 @@ package conversion;
 
 public class Conversor {
 
-	private final static double KM_A_MILLAS = 0.621371;
-	private final static double MB_A_ATM = 0.000986923;
-	private final static double CAL_A_JULIOS = 4.1868;
+	// Atributos-------------------------------------------------
+	private static final double KM_A_MILLAS = 0.621371;
+	private static final double MB_A_ATM = 0.000986923;
+	private static final double CAL_A_JULIOS = 4.1862;
 
+	// Constructor implicito-------------------------------------
+	
+	public Conversor() {
+
+	}
+
+	// Metodos---------------------------------------------------
 	public static double millas(double km) {
-		return KM_A_MILLAS * km;
+		return km * KM_A_MILLAS;
+	}
+
+	public static double km(double millas) {
+		return millas / KM_A_MILLAS;
+	}
+
+	public static double atmosferas(double milibares) {
+		return milibares * MB_A_ATM;
+	}
+
+	public static double milibares(double atmosferas) {
+		return atmosferas / MB_A_ATM;
+	}
+
+	public static double julios(double calorias) {
+		return calorias * CAL_A_JULIOS;
+	}
+
+	public static double calorias(double julios) {
+		return julios / CAL_A_JULIOS;
 	}
 
 }
---
+++ Tema5Proyecto2/src/inicio/Inicio.java
@@ -0,0 +1,35 @@
+package inicio;
+
+import java.util.Scanner;
+
+import conversion.Conversor;
+
+public class Inicio {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\nConversor de Kilometros a Millas:");
+        System.out.println("=================================");
+        System.out.println("\nDime un número con la unidad Kilómetros y te lo convertiré en Millas");
+        double km = sc.nextDouble();
+        double millas = Conversor.millas(km);
+        System.out.println("\n" + km + " kilómetros son " + millas + " millas.");
+
+        System.out.println("\nConversor de Milibares a Atmósferas:");
+        System.out.println("====================================");
+        System.out.println("\nDime un número con la unidad Milibares y te lo convertiré en Atmósferas");
+        double milibares = sc.nextDouble();
+        double atmosferas = Conversor.atmosferas(milibares);
+        System.out.println("\n" + milibares + " milibares son " + atmosferas + " atmósferas.");
+
+        System.out.println("\nConversor de Calorias a Julios:");
+        System.out.println("===============================");
+        System.out.println("\nDime un número con la unidad Calorias y te lo convertiré en Julios");
+        double calorias = sc.nextDouble();
+        double julios = Conversor.julios(calorias);
+        System.out.println("\n" + calorias + " calorías son " + julios + " julios.");
+
+        sc.close();
+    }
+}
