bb476b4e453a971d0829e777e359979ea51514c9
profesorderedes <profesorderedes@gmail.com>
2024-02-01 15:50:07 +0100

    jue 01 feb 2024 15:50:07 CET

--- Tema5Proyecto4/src/inicio/Inicio.java
+++ Tema5Proyecto4/src/inicio/Inicio.java
@@ -3,43 +3,51 @@ package inicio;
 import java.util.Scanner;
 
 import biblioteca.Autor;
+import biblioteca.Libro;
 
 public class Inicio {
 
+	private static Scanner entrada = new Scanner(System.in);
+
 	public static void main(String[] args) {
 
+		// Leemos los datos de los dos autores del libro.
+		
+
+		Autor autor1 = leerAutor();
 		System.out.println("\nDatos del autor: \n" + autor1.toString());
+
+		Autor autor2 = leerAutor();
+		System.out.println("\nDatos del autor: \n" + autor2.toString());
 
 		// Leemos los datos de un libro (suponemos que del autor anterior) y los
 		// guardamos en un objeto Libro.
 
-//		System.out.println("\n******* ALTA DE LIBRO *******");
-//
-//		// Limpiamos el buffer de entrada para evitar que se salte la siguiente lectura.
-//		entrada.nextLine();
-//
-//		System.out.print("Título: ");
-//		String titulo = entrada.nextLine();
-//
-//		System.out.print("ISBN: ");
-//		String isbn = entrada.nextLine();
-//
-//		System.out.print("Año actual: ");
-//		anyo = entrada.nextInt();
-//
-//		Libro libro1 = new Libro(titulo, isbn, autor1);
-//		Libro.setAnyoActual(anyo);
-//
-//		System.out.println("\nDatos del libro: \n" + libro1.toString());
+		System.out.println("\n******* ALTA DE LIBRO *******");
+
+		System.out.print("Título: ");
+		String titulo = entrada.nextLine();
+
+		System.out.print("ISBN: ");
+		String isbn = entrada.nextLine();
+
+		System.out.print("Año actual: ");
+		int anyo = entrada.nextInt();
+
+		// Creo un array con los autores del libro.
+		Autor[] autores = { autor1, autor2 };
+
+		Libro libro1 = new Libro(titulo, isbn, autores);
+		Libro.setAnyoActual(anyo);
+
+		System.out.println("\nDatos del libro: \n" + libro1.toString());
 
 	}
 
 	// Leemos los datos de un autor y los guardamos en un objeto de la clase Autor.
-	private Autor leerAutor() {
+	private static Autor leerAutor() {
 
-		Scanner entrada = new Scanner(System.in);
-
-		System.out.println("******* ALTA DE AUTOR *******");
+		System.out.println("\n******* ALTA DE AUTOR *******");
 
 		System.out.print("Nombre del autor: ");
 		String nombre = entrada.nextLine();
@@ -50,8 +58,15 @@ public class Inicio {
 		System.out.print("Año de nacimiento del autor: ");
 		int anyo = entrada.nextInt();
 
+		// Vaciamos el buffer de entrada para evitar que se salte el próximo nextLine().
+		entrada.nextLine();
+
 		return new Autor(nombre, email, anyo);
 
 	}
 
 }
+
+
+
+
