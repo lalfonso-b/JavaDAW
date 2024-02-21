21917933c74c9c9cf7bd19a57707f3aac0a37afb
profesorderedes <profesorderedes@gmail.com>
2024-02-21 19:58:50 +0100

    mié 21 feb 2024 19:58:50 CET

--- Tema6/src/prac1/Main.java
+++ Tema6/src/prac1/Main.java
@@ -1,14 +1,21 @@
 package prac1;
 
 public class Main {
-	
+
 	public static void main(String[] args) {
-		
+
 //		Medio me1 = new Medio(); Da error porque Medio ahora es abstracta
-		
+
 //		Mp3 mp1 = new Mp3();
 //		Ogg o1 = new Ogg();
-		
+
+		Medio m1 = new Mp3("Lobo hombre en París", 180, "La Unión", "Pop", 1);
+
+		m1.reproducir();
+		((Mp3) m1).guardar();
+
+		Guardable g1 = new Ogg("Carmen", 1000, "Bizet", "Ópera", 2);
+
 	}
 
 }
