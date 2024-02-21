d813319d7d7d8db5c312cf40a11e4c89a7cb3251
profesorderedes <profesorderedes@gmail.com>
2024-02-21 20:55:18 +0100

    mié 21 feb 2024 20:55:18 CET

--- Tema6/src/ejemplo06/Main.java
+++ Tema6/src/ejemplo06/Main.java
@@ -12,43 +12,44 @@ public class Main {
 
 		System.out.println("Escribe un nº entero: ");
 		String numero = entrada.nextLine();
-		
+
 		int num;
+
+		// Colocamos en un bloque try instrucciones que pueden lanzar un error.
 		try {
-			
+
+			// La siguiente instrucción puede lanzar un error (NumberFormatException) si lo
+			// que escribe el usuario no es un entero.
 			num = Integer.parseInt(numero);
+
+			// La siguiente instrucción puede lanzar un error (ArithmeticException) si lo
+			// que escribe el usuario es un 0.
 			System.out.println("La división de 20 entre " + num + " es " + 20 / num);
-			
+
 		} catch (NumberFormatException nfe) {
-			
+			// Esto se ejecutará si se ha producido un error de tipo NumberFormatException.
 			System.out.println("¡Tienes que escribir un número entero!");
 			
+			// Ahora el programa saltará fuera del bloque try-catch.
+
 		} catch (ArithmeticException ae) {
+			// Esto se ejecutará si se ha producido un error de tipo ArithmeticException.
 			
 			System.out.println("¡No se puede dividir por cero!");
+
+			// Ahora el programa saltará fuera del bloque try-catch.
 			
 		} catch (Exception e) {
-			
-			System.out.println("Se ha producido un error inesperado. El programa se cerrará.");
+			// Esto se ejecutará si se ha producido un error de cualquier tipo.
+
+			System.out.println("Se ha producido un error inesperado.");
 			System.out.println(e.toString());
-			System.exit(0);
 			
 		}
 
+		// Este código se ejecutará tras el try-catch aunque se haya producido una excepción.
 		System.out.println("\nAhora seguiría el código fuera del catch...");
 
 	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
--- Tema6/src/ejemplo06/Main2.java
+++ Tema6/src/ejemplo06/Main2.java
@@ -12,39 +12,35 @@ public class Main2 {
 
 		System.out.println("Escribe un nº entero: ");
 		String numero = entrada.nextLine();
-		
+
 		int num;
 		try {
-			
+
 			num = Integer.parseInt(numero);
 			System.out.println("La división de 20 entre " + num + " es " + 20 / num);
-			
+
 		} catch (NumberFormatException | ArithmeticException e) {
-			
+
 			System.out.println("¡Tienes que escribir un número entero distinto de 0!");
-			
+
 		} catch (Exception e) {
-			
+
 			System.out.println("Se ha producido un error inesperado. El programa se cerrará.");
 			System.out.println(e.toString());
-			System.exit(0);
-			
+			return;
+
+		} finally {
+			// El bloque finally se ejecutará tanto si ha habido una excepción como si no.
+			// Observar que tiene prioridad sobre el return del último catch.
+
+			System.out.println("Bien o mal, el try-catch ha finalizado.");
 		}
 
+		// Esto se ejecutará si no se produce ningún error o si se produce de tipo
+		// NumberFormatException o Arithmetic Exception. En el caso de que se produzca
+		// un Exception no se ejecutará, por el return.
 		System.out.println("\nAhora seguiría el código fuera del catch...");
 
 	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
