97653498271fd3255e0775033a48bcb9e719d1e0
profesorderedes <profesorderedes@gmail.com>
2024-03-05 20:49:22 +0100

    mar 05 mar 2024 20:49:21 CET

--- Tema7/src/ejemplos/Ejemplo04JComboBox.java
+++ Tema7/src/ejemplos/Ejemplo04JComboBox.java
@@ -1,10 +1,17 @@
 package ejemplos;
 
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 
+import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class Ejemplo04JComboBox extends JFrame {
 
@@ -14,7 +21,19 @@ public class Ejemplo04JComboBox extends JFrame {
 		setSize(400, 300);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-		setLayout(new FlowLayout());
+		setLayout(new FlowLayout(FlowLayout.LEFT));
+
+		try {
+			// Aplicamos el tema (look and feel) Nimbus, que ya viene incluído en la instalación de Java.
+			UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
+
+			// Esta instrucción asegura que el tema se aplique correctamente a toda la
+			// interfaz de usuario.
+			SwingUtilities.updateComponentTreeUI(this);
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+				| UnsupportedLookAndFeelException e) {
+			System.out.println("Error al cambiar el Look and Feel.");
+		}
 
 		JLabel lblProvincia = new JLabel("Selecciona tu provincia preferida:");
 		JComboBox<String> cmbProvincias = new JComboBox<>();
@@ -25,8 +44,29 @@ public class Ejemplo04JComboBox extends JFrame {
 		cmbProvincias.addItem("Jaén");
 		cmbProvincias.addItem("Albacete");
 
+		cmbProvincias.setPreferredSize(new Dimension(300, 20));
+
+		JLabel lblDescripcion = new JLabel("¿Por qué es tu provincia favorita?");
+
+		JTextArea txtDescripcion = new JTextArea();
+
+		// Hacemos que las líneas de texto no sobrepasen el ancho del TextArea.
+		txtDescripcion.setLineWrap(true);
+
+		// Hacemos que al saltar de línea automáticamente no se corten las palabras.
+		txtDescripcion.setWrapStyleWord(true);
+
+		JScrollPane scrPanel = new JScrollPane(txtDescripcion);
+		scrPanel.setPreferredSize(new Dimension(350, 80));
+
+		JButton btnGuardar = new JButton("Guardar");
+		btnGuardar.setPreferredSize(new Dimension(150, 35));
+
 		add(lblProvincia);
 		add(cmbProvincias);
+		add(lblDescripcion);
+		add(scrPanel);
+		add(btnGuardar);
 
 		setVisible(true);
 
---
+++ Tema7/src/ejemplos/Ejemplo05DetectarLookAndFeels.java
@@ -0,0 +1,20 @@
+package ejemplos;
+
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+
+public class Ejemplo05DetectarLookAndFeels extends JFrame {
+	
+	public Ejemplo05DetectarLookAndFeels() {
+		
+		UIManager.LookAndFeelInfo[] laf = UIManager.getInstalledLookAndFeels();
+		for (int i = 0; i < laf.length; i++) {
+			System.out.println("Class name: " + laf[i].getClassName());
+			System.out.println("Name: " + laf[i].getName() + "\n");
+		}
+	}
+
+	public static void main(String[] arguments) {
+		Ejemplo05DetectarLookAndFeels dialog = new Ejemplo05DetectarLookAndFeels();
+	}
+}
