6b688ff351b8abc27e2c5f06eaef2dbe4b6420b2
profesorderedes <profesorderedes@gmail.com>
2024-03-06 20:29:56 +0100

    mié 06 mar 2024 20:29:56 CET

---
+++ Tema7/src/hoja1/Ejer02.java
@@ -0,0 +1,15 @@
+package hoja1;
+
+import javax.swing.JOptionPane;
+
+public class Ejer02 {
+
+	public static void main(String[] args) {
+
+		JOptionPane.showInputDialog(null, "Escribe tu DNI:", "Confirmación de identidad",
+
+				JOptionPane.QUESTION_MESSAGE);
+
+	}
+
+}
