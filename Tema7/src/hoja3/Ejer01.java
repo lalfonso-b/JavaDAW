61a1a3005172510f03586fb27ed009f995bb5667
profesorderedes <profesorderedes@gmail.com>
2024-03-12 18:46:29 +0100

    mar 12 mar 2024 18:46:28 CET

---
+++ Tema7/src/hoja3/Ejer01.java
@@ -0,0 +1,52 @@
+package hoja3;
+
+import java.awt.BorderLayout;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JToolBar;
+
+public class Ejer01 extends JFrame {
+
+	public Ejer01() {
+
+		super("Planificador personal");
+		setSize(400, 200);
+		setLayout(new BorderLayout());
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+		JButton btnNuevo = new JButton(new ImageIcon("iconos/1.png"));
+		JButton btnNivel = new JButton(new ImageIcon("iconos/11.png"));
+		JButton btnGuardar = new JButton(new ImageIcon("iconos/22.png"));
+		JButton btnBombilla = new JButton(new ImageIcon("iconos/3.png"));
+		JButton btnCalendario = new JButton(new ImageIcon("iconos/44.png"));
+		JButton btnFoto = new JButton(new ImageIcon("iconos/66.png"));
+		JButton btnTele = new JButton(new ImageIcon("iconos/67.png"));
+		JButton btnConfiguracion = new JButton(new ImageIcon("iconos/73.png"));
+
+		JToolBar tlbPlanificador = new JToolBar();
+		tlbPlanificador.add(btnNuevo);
+		tlbPlanificador.add(btnNivel);
+		tlbPlanificador.add(btnGuardar);
+		tlbPlanificador.add(btnBombilla);
+		tlbPlanificador.addSeparator();
+
+		tlbPlanificador.add(btnCalendario);
+		tlbPlanificador.add(btnFoto);
+		tlbPlanificador.add(btnTele);
+		tlbPlanificador.addSeparator();
+
+		tlbPlanificador.add(btnConfiguracion);
+
+		add(tlbPlanificador, BorderLayout.NORTH);
+
+		setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new Ejer01();
+
+	}
+
+}
