797adc12be505427e8868f29eeac5348a5aab00b
profesorderedes <profesorderedes@gmail.com>
2024-03-12 19:50:00 +0100

    mar 12 mar 2024 19:50:00 CET

---
+++ Tema7/src/ejemplos/Ejemplo13GridLayout.java
@@ -0,0 +1,46 @@
+package ejemplos;
+
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class Ejemplo13GridLayout extends JFrame {
+
+	public Ejemplo13GridLayout() {
+
+		super("Stream Deck");
+		setSize(600, 310);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+		setLayout(new GridLayout(2, 4, 10, 10));
+
+		setLocationRelativeTo(null);
+
+		JButton btnStream = new JButton("Stream");
+		JButton btnCamara = new JButton("Cámara");
+		JButton btnSubirVolumen = new JButton("Subir volumen");
+		JButton btnBajarVolumen = new JButton("Bajar volumen");
+		JButton btnPublicidad = new JButton("Publicidad");
+		JButton btnSilenciar = new JButton("Silenciar micro");
+		JButton btnEscena1 = new JButton("Escena 1");
+		JButton btnEscena2 = new JButton("Escena 2");
+
+		add(btnStream);
+		add(btnCamara);
+		add(btnSubirVolumen);
+		add(btnBajarVolumen);
+		add(btnPublicidad);
+		add(btnSilenciar);
+		add(btnEscena1);
+		add(btnEscena2);
+
+		setVisible(true);
+
+	}
+
+	public static void main(String[] args) {
+		new Ejemplo13GridLayout();
+	}
+
+}
---
+++ Tema7/src/ejemplos/Ejemplo14GridLayout.java
@@ -0,0 +1,52 @@
+package ejemplos;
+
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Ejemplo14GridLayout extends JFrame {
+
+	public Ejemplo14GridLayout() {
+
+		super("Biblioteca");
+		setSize(400, 300);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+		setLayout(new GridLayout(1, 2));
+
+		setLocationRelativeTo(null);
+
+		// Parte izquierda
+		JPanel panelIzquierdo = new JPanel();
+
+		JLabel lblTitulo = new JLabel("Título");
+		JTextField txtTitulo = new JTextField(20);
+
+		JLabel lblAutor = new JLabel("Autor");
+		JTextField txtAutor = new JTextField(20);
+
+		panelIzquierdo.add(lblTitulo);
+		panelIzquierdo.add(txtTitulo);
+		panelIzquierdo.add(lblAutor);
+		panelIzquierdo.add(txtAutor);
+
+		// Parte derecha
+		JButton btnAlta = new JButton("Alta");
+
+		// Ventana
+		add(panelIzquierdo);
+		add(btnAlta);
+
+		setVisible(true);
+
+	}
+
+	public static void main(String[] args) {
+		new Ejemplo14GridLayout();
+	}
+
+}
