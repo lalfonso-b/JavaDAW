471108a41ef384fdd2921111b71f7e4f45cd9e9f
profesorderedes <profesorderedes@gmail.com>
2024-03-21 16:24:11 +0100

    jue 21 mar 2024 16:24:11 CET

---
+++ Tema7/src/ejemplos3/Ejemplo21KeyListener.java
@@ -0,0 +1,98 @@
+package ejemplos3;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Ejemplo21KeyListener extends JFrame {
+
+	private int x = 375, y = 240;
+
+	public Ejemplo21KeyListener() {
+
+		super("Eventos de teclado");
+
+		setSize(400, 300);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLayout(null);
+
+		setLocationRelativeTo(null);
+
+		JLabel lblCorazon = new JLabel(new ImageIcon("iconos/15.png"));
+		lblCorazon.setBounds(x, y, 24, 24);
+
+		addKeyListener(new KeyListener() {
+
+			@Override
+			public void keyTyped(KeyEvent arg0) {
+
+				// NO lo usamos
+
+			}
+
+			@Override
+			public void keyReleased(KeyEvent arg0) {
+
+				// NO lo usamos
+
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+
+				int codigoTecla = e.getKeyCode();
+
+				int velocidad = 3;
+
+				if (codigoTecla == KeyEvent.VK_UP) {
+
+					if (y > 0) {
+						y = y - velocidad;
+					}
+
+				} else if (codigoTecla == KeyEvent.VK_DOWN) {
+
+					if (y < 240) {
+						y = y + velocidad;
+					}
+
+				} else if (codigoTecla == KeyEvent.VK_LEFT) {
+
+					if (x > -23) {
+						x = x - velocidad;
+					} else {
+						x = 400;
+					}
+
+				} else if (codigoTecla == KeyEvent.VK_RIGHT) {
+
+					if (x < 400) {
+						x = x + velocidad;
+					} else {
+						x = -23;
+					}
+
+				}
+
+				lblCorazon.setBounds(x, y, 24, 24);
+
+			}
+
+		});
+
+		add(lblCorazon);
+
+		setVisible(true);
+
+	}
+
+	public static void main(String[] args) {
+
+		new Ejemplo21KeyListener();
+
+	}
+
+}
---
+++ Tema7/src/ejemplos3/Ejemplo22MouseListener.java
@@ -0,0 +1,85 @@
+package ejemplos3;
+
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Ejemplo22MouseListener extends JFrame {
+
+	public Ejemplo22MouseListener() {
+
+		super("Mouse Listener");
+		setSize(350, 300);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+		setLayout(new FlowLayout(FlowLayout.LEFT));
+
+		setLocationRelativeTo(null);
+
+		JLabel lblCoordenadas = new JLabel("¡Haz clic en esta ventana!");
+		JLabel lblBoton = new JLabel("");
+		
+		addMouseListener(new MouseListener() {
+			
+			@Override
+			public void mouseReleased(MouseEvent e) {
+				System.out.println("mouseReleased()");
+				
+				//getContentPane().setBackground(Color.BLACK);
+				
+			}
+			
+			@Override
+			public void mousePressed(MouseEvent e) {
+				System.out.println("mousePressed()");
+//				getContentPane().setBackground(Color.RED);
+				
+			}
+			
+			@Override
+			public void mouseExited(MouseEvent e) {
+				System.out.println("mouseExited()");
+//				getContentPane().setBackground(Color.GREEN);
+			}
+			
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				System.out.println("mouseEntered()");
+//				getContentPane().setBackground(Color.BLUE);
+			}
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				System.out.println("mouseClicked()");
+				int x = e.getX();
+				int y = e.getY();
+				lblCoordenadas.setText("Coordenadas: (" + x + ", " + y + ")");
+				
+				int boton = e.getButton();
+				if(boton == MouseEvent.BUTTON1) {
+					lblBoton.setText("Has pulsado el botón izquierdo");
+				}else if(boton == MouseEvent.BUTTON3) {
+					lblBoton.setText("Has pulsado el botón derecho");
+				}else if(boton == MouseEvent.BUTTON2) {
+					lblBoton.setText("Has pulsado la rueda del ratón");
+				}
+				
+			}
+		});
+
+		add(lblCoordenadas);
+		add(lblBoton);
+
+		setVisible(true);
+
+	}
+
+	public static void main(String[] args) {
+		new Ejemplo22MouseListener();
+	}
+
+}
