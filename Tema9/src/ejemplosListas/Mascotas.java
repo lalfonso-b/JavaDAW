ea59604ce1b4e6ab64d646a9975e492bcaf59c1c
profesorderedes <profesorderedes@gmail.com>
2024-05-07 17:26:17 +0200

    mar 07 may 2024 17:26:17 CEST

---
+++ Tema9/src/ejemplosListas/Mascotas.java
@@ -0,0 +1,75 @@
+package ejemplosListas;
+
+import java.util.Objects;
+
+public class Mascotas {
+	private String nombre;
+	private String raza;
+	private int edad;
+
+	public Mascotas(String nombre, String raza, int edad) {
+		super();
+		this.nombre = nombre;
+		this.raza = raza;
+		this.edad = edad;
+
+	}
+
+	// TODO: Crear un método equals que compare dos mascotas mirando sus tres
+	// atributos:
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getRaza() {
+		return raza;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+
+		if (obj == null) {
+			return false;
+		}
+
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+
+		// Casting de objeto obj para poder después comparar:
+		Mascotas other = (Mascotas) obj;
+
+		// Comparación:
+		return edad == other.edad && Objects.equals(nombre, other.nombre) && Objects.equals(raza, other.raza);
+
+	}
+
+	public static void main(String[] args) {
+
+		// Creamos dos objetos de tipo Mascotas con los mismos atributos
+		Mascotas mascota1 = new Mascotas("Jazz", "Labrador", 10);
+		Mascotas mascota2 = new Mascotas("Jazz", "Labrador", 10);
+
+		// Probamos si los objetos son iguales usando el método equals()
+		boolean sonIguales = mascota1.equals(mascota2);
+		System.out.println("¿Las mascotas son iguales? " + sonIguales); // Debería salir "true"
+
+		// Creamos dos objetos de tipo Mascotas con diferentes atributos
+		Mascotas mascota3 = new Mascotas("Funky", "Pitbull", 3);
+		Mascotas mascota4 = new Mascotas("Jazz", "Labrador", 10);
+
+		// Probamos si los objetos son iguales usando el método equals
+		sonIguales = mascota3.equals(mascota4);
+		System.out.println("¿Las mascotas son iguales? " + sonIguales); // Debería salir "false"
+
+	}
+
+}
