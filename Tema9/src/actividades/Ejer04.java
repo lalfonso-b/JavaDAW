3d01374435593e81bc52ca1fa10362b032c7c46f
profesorderedes <profesorderedes@gmail.com>
2024-05-10 19:38:03 +0200

    vie 10 may 2024 19:38:03 CEST

--- Tema9/src/actividades/Ejer04.java
+++ Tema9/src/actividades/Ejer04.java
@@ -3,27 +3,24 @@ package actividades;
 public class Ejer04 {
 
 	public static void main(String[] args) {
-		
+
 		int num1 = 20;
 		int num2 = 6;
-		
-		System.out.println(num1 + " divido entre " + num2 + " es " + division(num1,num2) + " (número entero).");
-		
+
+		System.out.println(num1 + " divido entre " + num2 + " es " + division(num1, num2) + " (número entero).");
+
 	}
-	
+
 	private static int division(int dividendo, int divisor) {
-		
-		int cantidadRestas = 0;
-		
-		if(dividendo >= divisor) {
-			
-			cantidadRestas++;
-			
-			return division((dividendo - divisor), divisor) + cantidadRestas;
-			
+
+		if (dividendo >= divisor) {
+
+			// System.out.println("division(" + (dividendo-divisor + ", " + divisor + ")"));
+			return division((dividendo - divisor), divisor) + 1;
+
 		}
-		
+
 		return 0;
 	}
-	
+
 }
