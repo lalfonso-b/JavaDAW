d3693c858ce83321f323892a77b07d7c2c7c8e2f
profesorderedes <profesorderedes@gmail.com>
2024-05-14 17:54:01 +0200

    mar 14 may 2024 17:54:00 CEST

---
+++ Tema9/src/actividades/Ejer06.java
@@ -0,0 +1,36 @@
+package actividades;
+
+public class Ejer06 {
+
+	public static void main(String[] args) {
+
+		int[] array = { -2,1, 9, 5, 7, 0,3, 2, 8, 6 };
+
+		int min = min(array, array.length);
+
+		System.out.println("El mínimo del array es: " + min);
+
+	}
+
+	private static int min(int[] numeros, int numElementos) {
+
+		// Caso base: Si la longitud del array es 1, devuelve el único elemento.
+
+		if (numElementos == 1) {
+
+			return numeros[0];
+
+		}
+
+		// Caso recursivo.
+
+		return Math.min(min(numeros, numElementos - 1), numeros[numElementos - 1]);
+
+	}
+
+}
+
+
+
+
+
