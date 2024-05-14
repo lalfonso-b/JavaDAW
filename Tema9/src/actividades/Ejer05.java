73f9eb44fbe5da68095b4ec0bfe372a0fee19ee4
profesorderedes <profesorderedes@gmail.com>
2024-05-14 17:24:36 +0200

    mar 14 may 2024 17:24:35 CEST

---
+++ Tema9/src/actividades/Ejer05.java
@@ -0,0 +1,22 @@
+package actividades;
+
+public class Ejer05 {
+
+	public static void main(String[] args) {
+
+		int n = 5;
+		System.out.println("La suma de 1 a "+ n + " es " + sumar(n));
+
+	}
+
+	public static int sumar(int num) {
+
+		if (num == 1) {
+			return 1;
+		}
+
+		return num + sumar(num - 1);
+
+	}
+
+}
