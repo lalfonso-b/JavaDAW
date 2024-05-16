5ef438296a8ea671e89b25c6f0bd5e88bf30b68e
profesorderedes <profesorderedes@gmail.com>
2024-05-16 16:08:03 +0200

    jue 16 may 2024 16:08:03 CEST

---
+++ Tema9/src/ejemplosMaps/Ejemplo04.java
@@ -0,0 +1,19 @@
+package ejemplosMaps;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Ejemplo04 {
+	
+	public static void main(String[] args) {
+		
+		// Creamos un mapa que asocie a cada cliente una ficha con información.
+		Map<Cliente, Ficha> registros = new HashMap<>();
+		
+		// Hemos añadido unos métodos equals() y hashCode() a la clase
+		// Cliente para poder usarla como clave.
+		
+		
+	}
+
+}
---
+++ Tema9/src/ejemplosMaps/Ficha.java
@@ -0,0 +1,39 @@
+package ejemplosMaps;
+
+public class Ficha {
+
+	private int anyoNacimiento;
+	private String nacionalidad;
+	private String visa;
+
+	public Ficha(int anyoNacimiento, String nacionalidad, String visa) {
+		this.anyoNacimiento = anyoNacimiento;
+		this.nacionalidad = nacionalidad;
+		this.visa = visa;
+	}
+
+	public int getAnyoNacimiento() {
+		return anyoNacimiento;
+	}
+
+	public void setAnyoNacimiento(int anyoNacimiento) {
+		this.anyoNacimiento = anyoNacimiento;
+	}
+
+	public String getNacionalidad() {
+		return nacionalidad;
+	}
+
+	public void setNacionalidad(String nacionalidad) {
+		this.nacionalidad = nacionalidad;
+	}
+
+	public String getVisa() {
+		return visa;
+	}
+
+	public void setVisa(String visa) {
+		this.visa = visa;
+	}
+
+}
