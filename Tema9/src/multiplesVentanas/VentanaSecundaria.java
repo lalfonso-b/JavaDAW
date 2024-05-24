fbb88a8b3104a99729c2a735374c31ff8aeca81a
profesorderedes <profesorderedes@gmail.com>
2024-05-24 16:19:35 +0200

    vie 24 may 2024 16:19:34 CEST

---
+++ Tema9/src/multiplesVentanas/VentanaSecundaria.java
@@ -0,0 +1,50 @@
+package multiplesVentanas;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class VentanaSecundaria extends JFrame {
+
+	private JTextField txtNombre;
+	private JTextField txtApellido;
+
+	public VentanaSecundaria() {
+
+		super("Ventana Secundaria");
+		setSize(270, 200);
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
+		setLayout(new FlowLayout(FlowLayout.LEFT));
+
+		JLabel lblNombre = new JLabel("Nombre:");
+		txtNombre = new JTextField(20);
+
+		JLabel lblApellido = new JLabel("Apellido:");
+		txtApellido = new JTextField(20);
+
+		JButton btnAceptar = new JButton("Aceptar");
+		btnAceptar.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+			}
+		});
+
+		add(lblNombre);
+		add(txtNombre);
+		add(lblApellido);
+		add(txtApellido);
+		add(btnAceptar);
+
+		setVisible(true);
+
+	}
+
+}
