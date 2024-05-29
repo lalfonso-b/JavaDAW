096330b01e851ebf45819de50048f979434aacac
profesorderedes <profesorderedes@gmail.com>
2024-05-29 19:31:40 +0200

    mié 29 may 2024 19:31:40 CEST

---
+++ Tema9/src/repaso/RenombrarCarpetas.java
@@ -0,0 +1,18 @@
+package repaso;
+
+import java.util.Scanner;
+
+public class RenombrarCarpetas {
+	
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		// Preguntar al usuario qué nombre quiere dar a la carpeta y al archivo, y cambiarles el nombre, usando File.renameTo().
+
+		
+		
+		
+	}
+
+}
