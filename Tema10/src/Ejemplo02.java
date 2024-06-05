5ba5ccf0662c0ac008fb22cf5b74f433c634839b
profesorderedes <profesorderedes@gmail.com>
2024-06-05 20:44:16 +0200

    mié 05 jun 2024 20:44:16 CEST

--- Tema10/src/Ejemplo02.java
+++ Tema10/src/Ejemplo02.java
@@ -4,6 +4,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
 public class Ejemplo02 {
 
@@ -15,6 +16,14 @@ public class Ejemplo02 {
 
 		listar("Alejandro Dumas", url, usuario, password);
 
+		Libro libro = new Libro(1007, "La ecuación de Dios", "Michio Kaku", 10, 1);
+		
+		// insertarLibro(url, usuario, password, libro);
+
+		// actualizarTitulo(url, usuario, password, 1002, "El Conde de Montecristo");
+		
+		borrarTabla(url, usuario, password);
+
 	}
 
 	private static void listar(String autor, String url, String usuario, String password) {
@@ -25,7 +34,7 @@ public class Ejemplo02 {
 				PreparedStatement preparedStatement = conexion.prepareStatement(sql)) {
 
 			preparedStatement.setString(1, autor);
-			
+
 			ResultSet resultados = preparedStatement.executeQuery();
 
 			System.out.println("LIBROS ESCRITOS POR: " + autor.toUpperCase());
@@ -44,6 +53,74 @@ public class Ejemplo02 {
 			System.out.println("Error de acceso a la base de datos.");
 			e.printStackTrace();
 		}
+	}
+
+	private static void insertarLibro(String url, String usuario, String password, Libro libro) {
+
+		String sql = "insert into libros values (?, ?, ?, ?, ?)";
+		try (Connection conexion = DriverManager.getConnection(url, usuario, password);
+				PreparedStatement preparedStatement = conexion.prepareStatement(sql)) {
+
+			preparedStatement.setInt(1, libro.getId());
+			preparedStatement.setString(2, libro.getTitulo());
+			preparedStatement.setString(3, libro.getAutor());
+			preparedStatement.setFloat(4, libro.getPrecio());
+			preparedStatement.setInt(5, libro.getUnidades());
+
+			int registrosModificados = preparedStatement.executeUpdate();
+			System.out.println("Se han modificado " + registrosModificados + " registros.");
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private static void actualizarTitulo(String url, String usuario, String password, int id, String titulo) {
+
+		String sql = "update libros set titulo=? where id=?";
+
+		try (Connection conexion = DriverManager.getConnection(url, usuario, password);
+				PreparedStatement preparedStatement = conexion.prepareStatement(sql)) {
+
+			preparedStatement.setString(1, titulo);
+			preparedStatement.setInt(2, id);
+
+			int registrosModificados = preparedStatement.executeUpdate();
+
+			System.out.println("Se han modificado " + registrosModificados + " registros.");
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private static void borrarTabla(String url, String usuario, String password) {
+
+		Scanner entrada = new Scanner(System.in);
+
+		System.out.println("¿Está seguro de querer eliminar el contenido de la tabla Libros? (s/n)");
+		String confirmacion = entrada.nextLine();
+
+		if (!confirmacion.equals("s")) {
+			System.out.println("No se ha borrado nada.");
+			return;
+		}
+
+		String sql = "delete from libros";
+
+		try (Connection conexion = DriverManager.getConnection(url, usuario, password);
+				PreparedStatement preparedStatement = conexion.prepareStatement(sql)) {
+
+			int registrosBorrados = preparedStatement.executeUpdate();
+
+			System.out.println("Se han borrado " + registrosBorrados + " registros.");
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
---
+++ Tema10/src/Ejemplo03.java
@@ -0,0 +1,14 @@
+
+public class Ejemplo03 {
+	
+	public static void main(String[] args) {
+		
+		String url = "jdbc:mysql://192.168.3.13/inversiones";
+		String usuario = "java";
+		String password = "Ageofempires2";
+		
+		
+		
+	}
+
+}
