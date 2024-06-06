9a70b3e4e874c5c510163ef0525e2a2491b62386
profesorderedes <profesorderedes@gmail.com>
2024-06-06 16:12:06 +0200

    jue 06 jun 2024 16:12:06 CEST

--- Tema10/src/Ejemplo03.java
+++ Tema10/src/Ejemplo03.java
@@ -11,7 +11,8 @@ public class Ejemplo03 {
 		String usuario = "java";
 		String password = "Ageofempires2";
 
-		addEmpresas(url, usuario, password);
+//		addEmpresas(url, usuario, password);
+		addAcciones(url, usuario, password);
 
 	}
 
@@ -47,7 +48,7 @@ public class Ejemplo03 {
 
 	}
 	
-	private static void addAcciones() {
+	private static void addAcciones(String url, String usuario, String password) {
 		
 		String[][] acciones = {
 				{"1","Carlos Pérez","1","100"},
@@ -55,6 +56,32 @@ public class Ejemplo03 {
 				{"3","José López","2","70"},
 				{"4","Marcos Jiménez","3","125"},
 		};
+		
+		String sql = "insert into acciones values  (?, ?, ?, ?)";
+
+		try (Connection conexion = DriverManager.getConnection(url, usuario, password);
+				PreparedStatement preparedStatement = conexion.prepareStatement(sql)) {
+
+			System.out.println("Insertando registros en la tabla Empresas...");
+			
+			for (int i = 0; i < acciones.length; i++) {
+				// Preparamos el preparedStatement para insertar la fila i del array en la base
+				// de datos.
+				preparedStatement.setInt(1, Integer.parseInt(acciones[i][0]));
+				preparedStatement.setString(2, acciones[i][1]);
+				preparedStatement.setInt(3, Integer.parseInt(acciones[i][2]));
+				preparedStatement.setInt(4, Integer.parseInt(acciones[i][3]));
+
+				// Insertamos en la base de datos.
+				int registrosInsertados = preparedStatement.executeUpdate();
+				System.out.println(registrosInsertados + " registros insertados.");
+			}
+
+		} catch (NumberFormatException e) {
+			System.out.println("Los campos id, id_empresa y cantidad deben ser enteros.");
+		} catch (SQLException e) {
+			System.out.println("Error al insertar datos.");
+		}
 		
 		
 	}
